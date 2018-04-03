8a1ab060472262edc9ea85e6f84990e4ade71938
Filip <filip.kozianski@outlook.com>
2018-04-03 20:43:01 +0200

    Rozpoczęcie pracy - podłączanie niezbędnych modułów, testowanie połączenia z bazą danych oracle przy użyciu modułu oracledb

---
+++ nodeScpService/scraps.js
@@ -0,0 +1,36 @@
+﻿var oracledb = require('oracledb');
+var dataRow = new Object();
+
+
+oracledb.getConnection(
+    {
+        user: "minimes_ff_wbr",
+        password: "Baza0racl3appl1cs",
+        connectString: "172.22.8.47/ORA"
+    },
+    function (err, connection)
+    {
+        if (err)
+        {
+            console.error(err);
+            return;
+        }
+        connection.execute(
+            "SELECT * FROM MACHINES@CUREMES",
+            {}, //WIAZANIE ZMIENNYCH
+            {//WŁAŚCIWOŚCI
+                //  resultSet: true
+                maxRows: 1000000
+            },
+            function (err, result)
+            {
+                if (err)
+                {
+                    console.error(err);
+                    return;
+                }
+
+                console.log(result.rows[1]);
+            });
+    });
+
--- nodeScpService/server.js
+++ nodeScpService/server.js
@@ -1,8 +1,15 @@
 'use strict';
 var http = require('http');
 var port = process.env.PORT || 1337;
+var scraps = require('./scraps');
+
+//var workbook = xlsx.readFile('PLAN_ROZPISKA.XLSM');
+//var worksheet = workbook.Sheets["ZESTAWIENIE_DOBA"];
+//var desired_cell = worksheet["L5"];
+//var desired_value = (desired_cell ? desired_cell.v : undefined);
+
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end(scraps.data);
 }).listen(port);
